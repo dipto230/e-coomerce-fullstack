0dbf4b75661cbf2b0cb06f39e02f57ae1231d321
dipto230 <diptoroy230305@gmail.com>
2025-07-22 23:48:37 +0530

    mobile navigation drawer done

--- src/components/Layout/CartDrawer.jsx
+++ src/components/Layout/CartDrawer.jsx
@@ -1,11 +1,12 @@
 
 import { IoMdClose } from 'react-icons/io';
 import { IoLogoInstagram } from 'react-icons/io5';
+import CartContents from '../Cart/CartContents';
 
 const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
    
   return (
-      <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
+      <div className={`fixed top-0 right-0 w-3/4 sm:w-[30rem] md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
           
           {/* Close button */}
           <div className='flex justify-end p-4'>
@@ -17,11 +18,12 @@ const CartDrawer = ({drawerOpen, toggleCartDrawer}) => {
           {/* Cart count with scrollbar  */}
           <div className='flex-grow p-4 overflow-y-auto'>
               <h2 className='text-xl font-semibold mb-4'>Your Cart</h2>
-              {/*components for cart area*/}
+              <CartContents />
               
           </div>
           {/*checkout button fixed at the bottom*/}
-          <div className='p=4 bg-white sticky bottom-0' >
+          <div className='p-4 bg-white sticky bottom-0'>
+
               <button className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition'>Checkout</button>
               <p className='text-sm tracking-tighter mt-2 text-center'>Shipping, taxes, and discount codes calculated at checkout.</p>
               
