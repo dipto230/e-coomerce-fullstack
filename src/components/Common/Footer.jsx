fee289d369ddbdcd68d1665f70dde41f13dc286e
dipto230 <diptoroy230305@gmail.com>
2025-07-23 12:32:05 +0530

    footer two section done

---
+++ src/components/Common/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Footer = () => {
+  return (
+      <footer className='border-t py-12'>
+          <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0'>
+              <div>
+                  <h3 className='text-lg text-gray-800 mb-4'>Newsletter</h3>
+                  <p>Be the first to hear about new products, exclusive events, and online offers</p>
+                  <p className='font-medium text-sm text-gray-600 mb-6'>Sign up and get 10% off your first order</p>
+
+
+                  {/* Newsletter form */}
+                  <form className='flex'>
+                      <input type="email" placeholder='Enter your email' className='p-3 w-full border-t border-l border-b border-gray-300 rounded-l-md focus:outline-none
+                      focus:ring-2 focus:ring-gray-500 transition-all required:
+                      '/>
+                      <button type='submit' className='bg-black text-white px-6 py-3 text-sm rounded-r-md hover:bg-gray-800 transition-all'>Subscribe</button>
+                  </form>
+
+              </div>
+              {/*Shop Links*/}
+              <div>
+                  <h3 className='text-lg  text-gray-800 mb-4'>Shop</h3>
+                  <ul className='space-y-2 text-gray-600'>
+                      <li>
+                          <Link to="#" className='hover:text-gray-600 transition-colors'>Men's top wear</Link>
+                      </li>
+                              <li>
+                          <Link to="#" className='hover:text-gray-600 transition-colors'>Men's bottom wear</Link>
+                      </li>
+                              <li>
+                          <Link to="#" className='hover:text-gray-600 transition-colors'>Women's top wear</Link>
+                      </li>
+                              <li>
+                          <Link to="#" className='hover:text-gray-600 transition-colors'>Women's bottom wear</Link>
+                      </li>
+                      
+                  </ul>
+              </div>
+          </div>
+          
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer'
 
 const UserLayout = () => {
   return (
       <>
           <Header />
+
+
+
+          <Footer/>
       </>
   )
 }
