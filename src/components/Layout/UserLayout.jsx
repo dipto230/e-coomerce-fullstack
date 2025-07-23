48b8caffa1ded9f05b2c531b5e38c5063b1175dd
dipto230 <diptoroy230305@gmail.com>
2025-07-23 16:39:56 +0530

    banner section done hero section

--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../Common/Header'
 import Footer from '../Common/Footer'
+import { Outlet } from 'react-router-dom'
 
 const UserLayout = () => {
   return (
       <>
-          <Header />
+      <Header />
+      <main>
+        <Outlet/>
+      </main>
 
 
 
