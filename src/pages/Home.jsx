3a82fabd847d2cb2ae450a8d574722cfe507db7e
dipto230 <diptoroy230305@gmail.com>
2025-07-25 15:08:50 +0530

    featured section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import GenderCollectionSection from '../components/Products/GenderCollectionSect
 import NewArrivals from '../components/Products/NewArrivals'
 import ProductDetails from '../components/Products/ProductDetails'
 import ProductGrid from '../components/Products/ProductGrid'
+import FeaturedCollection from '../components/Products/FeaturedCollection'
 
 const placeholderProducts = [
     {
@@ -49,6 +50,7 @@ const Home = () => {
               <ProductGrid products={placeholderProducts}/>
               
           </div>
+          <FeaturedCollection />
     </div>
   )
 }
