0bf2323c052f923a62d5c574003dada589ea9986
dipto230 <diptoroy230305@gmail.com>
2025-07-26 16:56:18 +0530

    login and sigup page layout done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import NewArrivals from '../components/Products/NewArrivals'
 import ProductDetails from '../components/Products/ProductDetails'
 import ProductGrid from '../components/Products/ProductGrid'
 import FeaturedCollection from '../components/Products/FeaturedCollection'
+import FeaturesSection from '../components/Products/FeaturesSection'
 
 const placeholderProducts = [
     {
@@ -51,6 +52,7 @@ const Home = () => {
               
           </div>
           <FeaturedCollection />
+          <FeaturesSection/>
     </div>
   )
 }
