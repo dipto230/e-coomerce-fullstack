33055ef0349fe30037932c5c4a5953577134f528
dipto230 <diptoroy230305@gmail.com>
2025-07-27 19:02:09 +0530

    filterside bar section done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from 'sonner'
 import Login from './pages/Login';
 import Register from './pages/Register'
 import Profile from './pages/Profile'
+import CollectionPage from './pages/CollectionPage'
 
 const App = () => {
   return (
@@ -17,7 +18,8 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
-            <Route path='profile' element={<Profile />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='collections/:collection' element={<CollectionPage/>}></Route>
           
         </Route>
         
--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
               {/* center - Navigation Links */}
             <div className='hidden md:flex space-x-6'>
 
-                  <Link to="#" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
+                  <Link to="/collections/all" className='text-gray-700 hover:text-black text-sm font-medium uppercase'>
                       Men
                   </Link>
 
